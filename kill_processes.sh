79a35e33524139da91c47484555ad39ae5be47fd
Mateus V Gasparino <matvalverde@gmail.com>
2024-03-28 04:37:44 +0800

    multiple devices time sync

--- kill_processes.sh
+++ kill_processes.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Reset chrony time
+/etc/init.d/chrony start |
+/etc/init.d/chrony stop |
+
 # First, kill all the processes using the cameras
 kill $(ps aux | grep '[c]amera' | awk '{print $2}')
 kill $(ps aux | grep '[i]mage' | awk '{print $2}')
